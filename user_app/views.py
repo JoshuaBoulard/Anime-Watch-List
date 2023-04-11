2cd55aa31000ac9f8ceddaee3bee3fed0542e02e
JoshuaBoulardJr <thegrey405@gmail.com>
2023-04-11 16:22:31 -0700

    PROJECT-002: Created psql db, django framework, react, and connected them.

---
+++ user_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def get_react_user(request):
+    react_view = open('static/index.html')
+    return HttpResponse(react_view)
